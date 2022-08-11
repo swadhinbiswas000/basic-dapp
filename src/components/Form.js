531e6a725f298fc0222e452a28630c133fcdcdee
Swadhin Biswas <swadhincpa@gmail.com>
2022-08-11 10:15:46 +0600

    Changing for details

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,49 +36,31 @@ import {
             color="white"
             borderRadius="lg"
             m={{ sm: 4, md: 16, lg: 10 }}
-            p={{ sm: 5, md: 5, lg: 16 }}>
+            p={{ sm: 5, md: 5, lg: 16 }}
+            >
+            <Heading style={{textAlign:"center"}}
+             m={{ sm: 1, md: 5, lg: 4 }}
+            >SB Token</Heading>
             <Box p={4}>
               <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
                 <WrapItem>
                   <Box>
-                    <Heading>Buy SB Token</Heading>
-                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                      Fill up the form below to contact
+                    <Button
+                          size="md"
+                          height="48px"
+                          width="200px"
+                          variant="solid"
+                          color="white"
+                          bg="#0D74FF"
+                          _hover={{ border: '2px solid #1C6FEB' }}>
+                          Connect Wallet
+                      </Button>
+                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="white">
+                      Wallet Address:
                     </Text>
-                    <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                      <VStack pl={0} spacing={3} alignItems="flex-start">
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="200px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                          +91-988888888
-                        </Button>
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="200px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-                          hello@abc.com
-                        </Button>
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="200px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
-                          Karnavati, India
-                        </Button>
-                      </VStack>
-                    </Box>
+                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="white">
+                      {}
+                    </Text>
                     <HStack
                       mt={{ lg: 10, md: 10 }}
                       spacing={5}
